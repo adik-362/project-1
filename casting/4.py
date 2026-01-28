411b1be81355b87043bcbf087d52ba0044458f1c
adik-362 <salauatadilet.77@gmail.com>
2026-01-28 22:20:31 +0600

    casting

---
+++ casting/4.py
@@ -0,0 +1,6 @@
+x = int(85)   # x will be 85
+y = int(3.2) # y will be 3.2
+z = int("8") # z will be 8
+print(x)
+print(y)
+print(z)
---
+++ casting/5.py
@@ -0,0 +1,6 @@
+x = str("s2") # x will be 's7'
+y = str(27)    # y will be '27'
+z = str(47.0)  # z will be '47.0'
+print(x)
+print(y)
+print(z)
